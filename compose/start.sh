930eef110054eb6087967c30870b9a097cc86cba
keythroy@gmail.com <jupyter@theia-20211020-141346.us-east1-b.c.elite-hold-329317.internal>
2021-10-22 02:42:54 +0000

    adicionando bash script

---
+++ compose/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common \
+    libgtk2.0-dev \
+    apache2-utils
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce
+# Linux post-install
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
