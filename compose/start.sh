afbea024c1680f5928bce1fdaffe46a3bd53a12b
keythroy@gmail.com <jupyter@theia-20211020-141346.us-east1-b.c.elite-hold-329317.internal>
2021-10-22 04:42:56 +0000

    ajustando bash

--- compose/start.sh
+++ compose/start.sh
@@ -21,12 +21,8 @@ sudo systemctl enable docker
 
 sudo apt-get install python3 python-pip
 
-curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh && \
-bash Anaconda3-2019.03-Linux-x86_64.sh && \
-source ~/.bashrc && \
-rm Anaconda3-2019.03-Linux-x86_64.sh && \
-tset && \
-conda update conda && \
-conda install -c conda-forge docker-compose \
-conda env create --name tcc_env --file=environment.yml && \
-conda activate tcc_env
+pip3 install docker-compose
+
+docker-compose -f nginx-compose.yaml up -d 
+
+docker-compose -f theia-compose.yaml up -d 
